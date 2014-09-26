44b9f14a55968e11c1f7759a7d6b328f5b6aaa20
yfsuse <yfsuse@gmail.com>
2014-09-26 08:54:25 +0800

    add time suffix to log file

--- script/alleria.py
+++ script/alleria.py
@@ -11,16 +11,25 @@ from common.producer import *
 from sort import SortTest
 from page import PageTest
 from common.parser import QueryProducer
-from time import ctime
+from time import ctime, strftime, localtime
 
 
 def run_sort(islp = False):
 
     case_list = get_sort_cases(islp)
-    err_handler = open('../log/sort.error.log', 'w')
-    suc_handler = open('../log/sort.success.log', 'w')
+    suffix = strftime("%Y-%m-%d-%H_%M_%S", localtime())
+
+    if islp:
+        error_log = '../log/sortlp.error.log.{0}'.format(suffix)
+        success_log = '../log/sortlp.success.log.{0}'.format(suffix)
+    else:
+        error_log = '../log/sort.error.log.{0}'.format(suffix)
+        success_log = '../log/sort.success.log.{0}'.format(suffix)
+
+    err_handler = open(error_log, 'w')
+    suc_handler = open(success_log, 'w')
     count = 0
-    for case in case_list[:1000]:
+    for case in case_list[:50]:
         positive_case_result = SortTest(case)
         negative_case = QueryProducer(case).get_reverse_order()
         negative_case_result = SortTest(negative_case)
@@ -37,10 +46,19 @@ def run_sort(islp = False):
 def run_page(islp = False):
 
     case_list = get_page_cases(islp)
-    err_handler = open('../log/page.error.log', 'w')
-    suc_handler = open('../log/page.success.log', 'w')
+    suffix = strftime("%Y-%m-%d-%H_%M_%S", localtime())
+
+    if islp:
+        error_log = '../log/pagelp.error.log.{0}'.format(suffix)
+        success_log = '../log/pagelp.success.log.{0}'.format(suffix)
+    else:
+        error_log = '../log/page.error.log.{0}'.format(suffix)
+        success_log = '../log/page.success.log.{0}'.format(suffix)
+
+    err_handler = open(error_log, 'w')
+    suc_handler = open(success_log, 'w')
     count = 0
-    for case in case_list[:1000]:
+    for case in case_list[:50]:
         negative_case_result = PageTest(case)
         positive_case = QueryProducer(case).get_no_page_query()
         positive_case_result = PageTest(positive_case)
@@ -54,8 +72,8 @@ def run_page(islp = False):
     suc_handler.close()
 
 if __name__ == '__main__':
-    run_sort()
     run_sort(islp=True)
+    run_sort()
     run_page()
     run_page(islp=True)
 
